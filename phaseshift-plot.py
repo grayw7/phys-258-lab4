5cc48c0f67fa1f892b0aa09c2b329df44f91d6aa
grayw7 <151585058+grayw7@users.noreply.github.com>
2025-02-09 20:16:05 -0500

    Update phaseshift-plot.py
    YESSIR IT FUCKING WORKS BABY

--- phaseshift-plot.py
+++ phaseshift-plot.py
@@ -21,31 +21,34 @@ def calculate_phase_points(omega_drives_rads, all_times, all_amps, all_drives):
 
         dt = np.mean(np.diff(time))
         N = len(time)
-        freqs = fftfreq(N, dt)  # Frequencies in Hz
+        freqs = fftfreq(N, dt)
         target_freq_hz = omega_drive_rads
         
+
         idx = np.argmin(np.abs(freqs - target_freq_hz))
         
+
         amp_phase = np.angle(fft(amp)[idx])
         drive_phase = np.angle(fft(drive)[idx])
         
-        #FIXED BY NORMILZING PHASE TO -pi -> pi
-        phase_diff = ((amp_phase - drive_phase) + np.pi) % (2 * np.pi) - np.pi
-
+        #HOLY THIS TOOK WAY TOO LONG TO FIGURE OUT 
+        phase_diff = np.angle(np.exp(1j * ((drive_phase - amp_phase) - np.pi)))
 
         
+
         phase_data.append((target_freq_hz, phase_diff))
     
     return np.array(phase_data)
 
-# Load data
+
 df = pd.read_csv('/Users/willgray/Downloads/part-2-data-12trails.csv')
+
 
 times = []
 amps = []
 drives = []
 
-for run in range(1, 12):
+for run in range(1, 12): 
     time_col = f'Time (s) Run #{run}'
     chA_col = f'Voltage, Ch A (V) Run #{run}'
     chB_col = f'Voltage, Ch B (V) Run #{run}'
@@ -54,18 +57,20 @@ for run in range(1, 12):
     amps.append(df[chA_col].values)
     drives.append(df[chB_col].values)
 
-# our driving frequency
-omega_drives_rads = np.array([0.36,0.46,0.56,0.66,0.76,0.86,0.96,1.06,1.16,1.26,1.36])
+# our driving force 
+omega_drives_hz = np.array([0.36,0.46,0.56,0.66,0.76,0.86,0.96,1.06,1.16,1.36,1.26])
+
+
 
 phase_points = calculate_phase_points(omega_drives_rads, times, amps, drives)
 
-# Print all points 
+
 print("\nPhase Shift Data Points:")
 print(f"{'Trial':<6} {'Frequency (Hz)':<15} {'Phase Shift (rad)':<15}")
 for i, (freq_hz, phase) in enumerate(phase_points):
     print(f"{i+1:<6} {freq_hz:<15.3f} {phase:<15.3f}")
 
-
+# Plot!!!
 plt.figure(figsize=(8, 5))
 plt.scatter(phase_points[:, 0], phase_points[:, 1], c='red', s=50, edgecolor='black')
 
